8d23e514d920cb8a40d4561cbb2f490215d780bb
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-04-22 18:32:23 +0200

    First Client Socket

---
+++ src/org/campus02/urldemo/FirstClientSocket.java
@@ -0,0 +1,37 @@
+package org.campus02.urldemo;
+
+import java.io.*;
+import java.net.Socket;
+
+public class FirstClientSocket {
+    public static void main(String[] args) {
+        try {
+
+            // Erzeuge einen neuen Socket und verbinde dich zum Server
+            Socket socket = new Socket("info.cern.ch", 80);
+
+            // lese Daten vom Server
+            InputStream read = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(read));
+
+            // sende Daten an den Server
+            OutputStream send = socket.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(send));
+
+            // sende einen Request an den Server
+            bw.write("GET / HTTP/1.1\r\n");
+            //bw.newLine();
+            bw.write("Host: info.cern.ch\r\n\r\n"); // !! Zeilumbrüche
+            bw.flush();
+
+            String html;
+            while ((html = br.readLine()) != null) {
+                System.out.println(html);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
