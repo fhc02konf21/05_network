78657b3ae722bcb939fa882320c5e3bc6ae0ccb5
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-04-22 20:52:09 +0200

    count clients on TimeServer

--- src/org/campus02/urldemo/networking/time/TimeServer.java
+++ src/org/campus02/urldemo/networking/time/TimeServer.java
@@ -12,14 +12,17 @@ public class TimeServer {
     public static void main(String[] args) {
         System.out.println("Erzeuge den Server");
         try(ServerSocket serverSocket = new ServerSocket(1111)) {
+            int clients = 0;
 
             // laufe unendlich
-            while (true) {
+            while (clients < 5) {
                 System.out.println("Warte auf Clients...");
                 try(Socket client = serverSocket.accept();
                     BufferedWriter bw = new BufferedWriter(
                             new OutputStreamWriter(client.getOutputStream()))) {
                     System.out.println("Mit dem Client verbunden");
+                    clients ++;
+                    System.out.println("clients: " + clients);
 
                     LocalDateTime now = LocalDateTime.now();
                     //bw.write(now.toString());
